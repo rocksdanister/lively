a5638352c8dd1f8a78cd7b50a4f8fdd175b31759
Dani John <rocksdanister@gmail.com>
2020-06-11 23:44:34 +0530

    .net core starting code
    todo:
    - setup uwp project class that derives from XamlApplication for hosting custom uwp controls.
    - Literally Everything else.

---
+++ src/livelywpf/livelywpf/Views/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using ModernWpf.Media.Animation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Xaml.Controls;
+
+namespace livelywpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ContentFrame.Navigate(typeof(livelywpf.Views.LibraryView), new Uri("Views/LibraryView.xaml", UriKind.Relative), new SuppressNavigationTransitionInfo());
+        }
+
+
+        private void MyNavView_ChildChanged(object sender, EventArgs e)
+        {
+            WindowsXamlHost windowsXamlHost = (WindowsXamlHost)sender;
+
+            Windows.UI.Xaml.Controls.NavigationView navView =
+                (Windows.UI.Xaml.Controls.NavigationView)windowsXamlHost.Child;
+
+            if (navView != null)
+            {
+                navView.OpenPaneLength = 50;
+                navView.IsPaneToggleButtonVisible = false;
+                navView.IsBackButtonVisible = NavigationViewBackButtonVisible.Collapsed;
+                navView.PaneDisplayMode = NavigationViewPaneDisplayMode.LeftCompact;
+                navView.MenuItems.Add(CreateMenu("Library", "library", Symbol.Library));
+                navView.MenuItems.Add(CreateMenu("Layout", "layout", Symbol.ViewAll));
+                navView.MenuItems.Add(CreateMenu("Playlist", "playlist", Symbol.SlideShow));
+                navView.MenuItems.Add(CreateMenu("About", "about", Symbol.Comment));
+                navView.ItemInvoked += NavView_ItemInvoked;
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            Windows.UI.Xaml.Controls.NavigationView navView =
+                (Windows.UI.Xaml.Controls.NavigationView)sender;
+
+            if (args.IsSettingsInvoked)
+            {
+                //ContentFrame.NavigationService.Navigate(new Uri("Views/SettingsView.xaml", UriKind.Relative));
+                ContentFrame.Navigate(typeof(livelywpf.Views.SettingsView), new Uri("Views/SettingsView.xaml", UriKind.Relative), new DrillInNavigationTransitionInfo());
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                //MessageBox.Show(navItemTag);
+                switch (navItemTag)
+                {
+                    case "library":
+                        ContentFrame.Navigate(typeof(livelywpf.Views.LibraryView), new Uri("Views/LibraryView.xaml", UriKind.Relative), new DrillInNavigationTransitionInfo());
+                        break;
+                    default:
+                        ContentFrame.Navigate(typeof(livelywpf.Views.LibraryView), new Uri("Views/LibraryView.xaml", UriKind.Relative), new DrillInNavigationTransitionInfo());
+                        break;
+                }
+            }
+        }
+
+        private Windows.UI.Xaml.Controls.NavigationViewItem CreateMenu(string menuName, string tag, Symbol icon)
+        {
+            Windows.UI.Xaml.Controls.NavigationViewItem item = new NavigationViewItem
+            {
+                Name = menuName,
+                Content = menuName,
+                Tag = tag,
+                Icon = new SymbolIcon(icon)
+            };
+            return item;
+        }
+
+    }
+}
---
+++ src/livelywpf/livelywpf/Views/SettingsView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace livelywpf.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : Page
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
