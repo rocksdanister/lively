49d99aac0146d5fff5e8585fa3029bcbbccfd294
Dani John <rocksdanister@gmail.com>
2020-06-21 22:33:53 +0530

    logger added
    - nlog logger initialisation code.
    - system information.

---
+++ src/livelywpf/livelywpf/Logger/NLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace livelywpf
+{
+    public class NLogger
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void SetupNLog()
+        {
+            throw new NotImplementedException();
+            /*
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = Path.Combine(PathData, "logfile.txt"), DeleteOldFileOnStartup = true };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            */
+        }
+
+        public static void LogHardwareInfo()
+        {
+            Logger.Info("Lively v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " " + CultureInfo.CurrentCulture.Name + "  64Bit:" + Environment.Is64BitProcess);
+            //Logger.Info("Portable build: " + App.isPortableBuild);
+            Logger.Info(SystemInfo.GetOSInfo());
+            Logger.Info(SystemInfo.GetCPUInfo());
+            Logger.Info(SystemInfo.GetGPUInfo());
+        }
+
+        public static void LogWin32Error(string msg = null)
+        {
+            //todo: throw equivalent win32 exception.
+            int err = System.Runtime.InteropServices.Marshal.GetLastWin32Error();
+            if (err != 0)
+            {
+                Logger.Error(msg + " HRESULT:" + err);
+            }
+        }
+    }
+}
---
+++ src/livelywpf/livelywpf/Logger/SystemInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Management;
+using System.Text;
+
+namespace livelywpf
+{
+    /// <summary>
+    /// Retrieve system information:- operating system version, cpu & gpu name.
+    /// </summary>
+    public static class SystemInfo
+    {
+        public static string GetGPUInfo()
+        {
+            try
+            {
+                using (ManagementObjectSearcher myVideoObject = new ManagementObjectSearcher("select * from Win32_VideoController"))
+                {
+                    var sb = new StringBuilder();
+                    foreach (ManagementObject obj in myVideoObject.Get())
+                    {
+                        sb.AppendLine("GPU: " + obj["Name"]);
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return "GPU: " + e.Message;
+            }
+        }
+
+        public static string GetCPUInfo()
+        {
+            try
+            {
+                using (ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor"))
+                {
+                    var sb = new StringBuilder();
+                    foreach (ManagementObject obj in myProcessorObject.Get())
+                    {
+                        sb.AppendLine("CPU: " + obj["Name"]);
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return "CPU: " + e.Message;
+            }
+        }
+
+        public static string GetOSInfo()
+        {
+            try
+            {
+                using (ManagementObjectSearcher myOperativeSystemObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem"))
+                {
+                    var sb = new StringBuilder();
+                    foreach (ManagementObject obj in myOperativeSystemObject.Get())
+                    {
+                        sb.AppendLine("OS: " + obj["Caption"] + " " + obj["Version"]);
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                return "OS: " + e.Message;
+            }
+        }
+    }
+}
