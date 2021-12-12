2798b717ec4e0e27052116bc7c5e2a9d964aa6ff
Dani John <rocksdanister@gmail.com>
2021-12-12 09:06:44 +0530

    Removed WM_SHOWLIVELY..
    - Minor refactoring.
    - Screensaver save data model class impl.

---
+++ src/livelywpf/livelywpf/Models/IScreensaverLayoutModel.cs
@@ -0,0 +1,12 @@
+﻿using livelywpf.Core;
+
+namespace livelywpf.Models
+{
+    public interface IScreensaverLayoutModel
+    {
+        WallpaperArrangement Arrangement { get; set; }
+        string LivelyInfoPath { get; set; }
+        LivelyScreen LivelyScreen { get; set; }
+        ScreensaverMode Mode { get; set; }
+    }
+}
