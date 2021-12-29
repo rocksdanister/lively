365f1d1a8d2569da9794363fd891db12cf0af8e3
Dani John <rocksdanister@gmail.com>
2021-12-29 16:39:58 +0530

    grpc wallpaperchangedevent

---
+++ src/Lively/Lively.Grpc.Client/WallpaperChangedEvent.cs
@@ -0,0 +1,53 @@
+﻿using Desktop;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lively.Grpc.Client
+{
+    class WallpaperChangedEvent
+    {
+        private DesktopService.DesktopServiceClient mClient;
+        private CancellationTokenSource mCancellationTokenSource;
+        private Task mMonitorTask = null;
+
+        public event EventHandler<int> WallpaperChanged;
+
+        public WallpaperChangedEvent(DesktopService.DesktopServiceClient client)
+        {
+            mClient = client;
+        }
+
+        public void Start()
+        {
+            mCancellationTokenSource = new CancellationTokenSource();
+            mMonitorTask = Task.Run(() => SubscribeWallpaperChangedServer(mCancellationTokenSource.Token));
+        }
+
+        public void Stop()
+        {
+            mCancellationTokenSource.Cancel();
+            mMonitorTask.Wait(1000);
+        }
+
+        private async Task SubscribeWallpaperChangedServer(CancellationToken token)
+        {
+            try
+            {
+                using var call = mClient.SubscribeWallpaperChanged(new Empty());
+                while (await call.ResponseStream.MoveNext(token))
+                {
+                    var response = call.ResponseStream.Current;
+                    WallpaperChanged?.Invoke(this, response.Count);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- src/Lively/Lively/IPC/DesktopService.cs
+++ src/Lively/Lively/IPC/DesktopService.cs
@@ -12,24 +12,62 @@ using Lively.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Lively.Core;
 using Lively.Core.Display;
+using System.Diagnostics;
 
 namespace Lively.IPC
 {
     internal class DesktopService : Desktop.DesktopService.DesktopServiceBase
     {
+        private readonly IDesktopCore desktopCore;
+        private readonly IDisplayManager displayManager;
+
+        public DesktopService(IDesktopCore desktopCore, IDisplayManager displayManager)
+        {
+            this.desktopCore = desktopCore;
+            this.displayManager = displayManager;
+        }
+
         public override Task<WallpaperResponse> SetWallpaper(WallpaperRequest request, ServerCallContext context)
         {
             //TEST
-            var core = App.Services.GetRequiredService<IDesktopCore>();
-            var display = App.Services.GetRequiredService<IDisplayManager>();
             var lm = ScanWallpaperFolder(request.LivelyInfoPath);//(@"C:\Users\rocks\AppData\Local\Lively Wallpaper_v2\Library\wallpapers\iqdvd4pt.jyo");
-            core.SetWallpaper(lm, display.PrimaryDisplayMonitor);
+            desktopCore.SetWallpaper(lm, displayManager.PrimaryDisplayMonitor);
 
             return Task.FromResult(new WallpaperResponse
             {
                 Status = true,
             });
         }
+
+        public override async Task SubscribeWallpaperChanged(Empty _, IServerStreamWriter<WallpaperChangedModel> responseStream, ServerCallContext context)
+        {
+            try
+            {
+                while (!context.CancellationToken.IsCancellationRequested)
+                {
+                    var tcs = new TaskCompletionSource<bool>();
+                    desktopCore.WallpaperChanged += WallpaperChanged;
+                    void WallpaperChanged(object s, EventArgs e)
+                    {
+                        desktopCore.WallpaperChanged -= WallpaperChanged;
+                        tcs.SetResult(true);
+                    }
+                    await tcs.Task;
+
+                    var response = new WallpaperChangedModel
+                    {
+                        Count = desktopCore.Wallpapers.Count,
+                    };
+                    await responseStream.WriteAsync(response);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+        }
+
+        #region helpers
 
         //TEST
         private ILibraryModel ScanWallpaperFolder(string folderPath)
@@ -74,5 +112,7 @@ namespace Lively.IPC
             }
             return null;
         }
+
+        #endregion //helpers
     }
 }
