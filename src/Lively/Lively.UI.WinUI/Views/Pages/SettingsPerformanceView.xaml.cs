131b5b4895c005993e55ce5d566a58be5cf5f4c1
Dani John <rocksdanister@gmail.com>
2022-02-01 21:30:37 +0530

    winui crappy navigation code..
    it works :)

--- src/Lively/Lively.UI.WinUI/Views/Pages/SettingsPerformanceView.xaml.cs
+++ src/Lively/Lively.UI.WinUI/Views/Pages/SettingsPerformanceView.xaml.cs
@@ -23,9 +23,9 @@ namespace Lively.UI.WinUI.Views.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SettingsView : Page
+    public sealed partial class SettingsPerformanceView : Page
     {
-        public SettingsView()
+        public SettingsPerformanceView()
         {
             this.InitializeComponent();
             this.DataContext = App.Services.GetRequiredService<SettingsViewModel>();
