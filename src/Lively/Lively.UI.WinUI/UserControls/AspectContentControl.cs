2f81f321a7627df440f71a4cc87ac306a5af8358
Dani John <rocksdanister@gmail.com>
2022-02-12 06:10:58 +0530

    adaptivegridview (community toolkit)

---
+++ src/Lively/Lively.UI.WinUI/UserControls/AspectContentControl.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace Lively.UI.WinUI.UserControls
+{
+    public class AspectContentControl : ContentControl
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return new Size(availableSize.Width, availableSize.Width * 0.56); //272/153
+        }
+
+    }
+}
