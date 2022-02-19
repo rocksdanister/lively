07b92ce2de3cf1d261f17d1fa594743126d4d668
Dani John <rocksdanister@gmail.com>
2022-02-19 23:44:37 +0530

    some refactoring of navview code..
    - started working on preview and thumbnail generation system

---
+++ src/Lively/Lively/Views/LibraryPreview.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lively.Views
+{
+    /// <summary>
+    /// Interaction logic for LibraryPreview.xaml
+    /// </summary>
+    public partial class LibraryPreview : Window
+    {
+        public LibraryPreview()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
