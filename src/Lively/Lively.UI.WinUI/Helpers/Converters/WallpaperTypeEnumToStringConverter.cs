3df6a8daf61d19084e4f691a5b9b6f42bd0ef0c0
Dani John <rocksdanister@gmail.com>
2022-04-21 08:09:28 +0530

    wallpaper localized typename converter

---
+++ src/Lively/Lively.UI.WinUI/Helpers/Converters/WallpaperTypeEnumToStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using Lively.Common;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lively.UI.WinUI.Helpers.Converters
+{
+    public sealed class WallpaperTypeEnumToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var targetValue = string.Empty;
+            try
+            {
+                var type = (WallpaperType)value;
+                targetValue = LocalizationUtil.GetLocalizedWallpaperCategory(type);
+            }
+            catch { }
+            return targetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
