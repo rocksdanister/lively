ee984c5a523635a03f381ad18c89ace681b71ef3
Dani John <rocksdanister@gmail.com>
2022-08-25 10:05:14 +0530

    Error infobar on addwallpaper dialog..
    - Converter for inforbar severity (Update check dialog.)
    - Fixed filepicker crashing when extension dropdown is opened.

---
+++ src/Lively/Lively.UI.WinUI/Helpers/Converters/StringToInfoBarSeverityConverter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lively.UI.WinUI.Helpers.Converters
+{
+    public class StringToInfoBarSeverityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value as string) switch
+            {
+                "Informational" => InfoBarSeverity.Informational,
+                "Success" => InfoBarSeverity.Success,
+                "Warning" => InfoBarSeverity.Warning,
+                "Error" => InfoBarSeverity.Error,
+                _ => InfoBarSeverity.Error,
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
