6585e5493f488318134c5410c2a04362de196576
Dani John <rocksdanister@gmail.com>
2023-04-25 21:13:00 +0530

    patch

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ let shaderUniforms = [
     u_time: { value: 0, type: "f" },
     u_brightness: { value: 0.75, type: "f" },
     u_resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight), type: "v2" },
-  },
+  }
 ];
 const quad = new THREE.Mesh(new THREE.PlaneGeometry(2, 2, 1, 1));
 let videoElement;
@@ -266,14 +266,16 @@ async function setScene(name, geometry = quad) {
         setScale(0.75);
       }
       break;
-    case "impulse": {
-      material = new THREE.ShaderMaterial({
-        uniforms: shaderUniforms[4],
-        vertexShader: vertexShader,
-        fragmentShader: await (await fetch("shaders/impulse.frag")).text(),
-      });
-      setScale(1);
-    }
+    case "impulse":
+      {
+        material = new THREE.ShaderMaterial({
+          uniforms: shaderUniforms[4],
+          vertexShader: vertexShader,
+          fragmentShader: await (await fetch("shaders/impulse.frag")).text(),
+        });
+        setScale(1);
+      }
+      break;
   }
   geometry.material = material;
   resize(); //update view
