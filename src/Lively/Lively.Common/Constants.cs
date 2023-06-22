950cfc2a90e54086b3a07ea26ab057061bd9fd37
Dani John <rocksdanister@gmail.com>
2023-06-22 06:57:19 +0530

    Refactoring and enhancemnets to depth prediction viewmodel..
    - ... avoid tempfiles, smoother UX
    - added some additional icon images (unused.)
    - added JFIF image support.

--- src/Lively/Lively.Common/Constants.cs
+++ src/Lively/Lively.Common/Constants.cs
@@ -46,7 +46,7 @@ namespace Lively.Common
         public static class MachineLearning
         {
             public static string BaseDir { get; } = Path.Combine(CommonPaths.AppDataDir, "ML");
-            public static string MiDaSDir { get; } = Path.Combine(BaseDir, "MiDaS");
+            public static string MiDaSDir { get; } = Path.Combine(BaseDir, "Midas");
             public static string MiDaSPath { get; } = Path.Combine(MiDaSDir, "midas_model_small_v2100.onnx");
         }
 
