cf934a01ddeffbd28321b8e670eb30832f24c91e
Dani John <rocksdanister@gmail.com>
2023-06-25 00:02:40 +0530

    tray-customize menu display scaling

--- src/Lively/Lively.UI.WinUI/Views/LivelyProperty/LivelyPropertiesTray.xaml.cs
+++ src/Lively/Lively.UI.WinUI/Views/LivelyProperty/LivelyPropertiesTray.xaml.cs
@@ -1,25 +1,8 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using Lively.Common.Helpers.Pinvoke;
+﻿using Lively.Common.Helpers.Pinvoke;
 using Lively.Grpc.Client;
 using Lively.Models;
-using Lively.UI.WinUI.Views.Pages;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
-using Microsoft.UI.Xaml.Navigation;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using WinUIEx;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -43,10 +26,13 @@ namespace Lively.UI.WinUI.Views.LivelyProperty
             var display = App.Services.GetRequiredService<IDisplayManagerClient>().PrimaryMonitor;
             if (display is not null)
             {
-                this.Height = display.WorkingArea.Height / 1.25f;
-                var left = display.WorkingArea.Right - this.Width - 5;
-                var top = display.WorkingArea.Bottom - this.Height - 10;
-                NativeMethods.SetWindowPos(this.GetWindowHandle(), -2, (int)left, (int)top, (int)this.Width, (int)this.Height, (int)NativeMethods.SetWindowPosFlags.SWP_SHOWWINDOW);
+                var dpi = NativeMethods.GetDpiForWindow(this.GetWindowHandle());
+                var scalingFactor = (float)dpi / 96;
+                var width = (int)(375 * scalingFactor);
+                var height = (int)(900 * scalingFactor);
+                var left = display.WorkingArea.Right - width - 5;
+                var top = display.WorkingArea.Bottom - height - 10;
+                NativeMethods.SetWindowPos(this.GetWindowHandle(), -2, left, top, width, height, (int)NativeMethods.SetWindowPosFlags.SWP_SHOWWINDOW);
             }
 
             contentFrame.Navigate(typeof(LivelyPropertiesView), model);
